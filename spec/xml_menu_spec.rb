15806ec3db18b11b7d4c00fa9b78e0fce261bdc2
DEkeSparta <deke@spartaglobal.com>
2018-10-09 16:35:55 +0100

    Completed first test and relevant code, for checking maximum price is less than £10

--- spec/xml_menu_spec.rb
+++ spec/xml_menu_spec.rb
@@ -7,7 +7,7 @@ describe "XML menu" do
   end
 
   it "no price should be more than £10" do
-    pending
+    expect(@xml_menu.get_prices.max).to be < 10
   end
 
   it "should have no item with calories over 1000 except for the full breakfast" do
--- xml_parsing.rb
+++ xml_parsing.rb
@@ -10,4 +10,21 @@ class GuiseppesMenu
 
   # Place your methods here
 
+  def get_prices
+    price_items = @menu.xpath("//price")
+    prices = []
+    price_items.each do |p|
+      prices.push p.text[1..-1].to_f
+    end
+
+    return prices
+  end
+
+  def get_foods
+    return @menu.xpath("//food")
+  end
+
 end
+
+xml = GuiseppesMenu.new
+puts xml.get_foods
